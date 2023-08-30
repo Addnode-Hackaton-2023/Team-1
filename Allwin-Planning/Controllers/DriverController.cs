8c61dc8bb6f45f3437443e9491b659b3dcf9c96b
Niklas Almered <niklas.almered@decerno.se>
2023-08-30 19:35:25 +0200

    Endpoint to get stops for optimisation

---
+++ Allwin-Planning/Controllers/DriverController.cs
@@ -0,0 +1,21 @@
+﻿using Allwin_Planning.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Allwin_Planning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        public Stops GetStops(Guid vehicleId, int dayOfWeek)
+        {
+            return new Stops()
+            {
+                Depot = new Depot(),
+                Pickups = new List<Pickup>(),
+                Delivery = new Delivery()
+            };
+        }
+    }
+}
