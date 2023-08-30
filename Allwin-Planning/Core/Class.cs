99789ad8e8cd380bffea080efbda75953609047d
Patrik Sjökvist <patrik.sjokvist@sgroup-solutions.se>
2023-08-30 19:38:28 +0200

    Added repository

---
+++ Allwin-Planning/Core/Class.cs
@@ -0,0 +1,8 @@
+﻿namespace Allwin_Planning.Core
+{
+	public enum PickupType
+	{
+		Train = 0,
+		Store = 1
+	}
+}
---
+++ Allwin-Planning/Core/Interfaces/IAllwinRepository.cs
@@ -0,0 +1,13 @@
+﻿using Allwin_Planning.Core.Entities;
+
+namespace Allwin_Planning.Core.Interfaces
+{
+	public interface IAllwinRepository
+	{
+		Task<List<Pickup>> GetPickups(Guid vehicleId);
+		Task<List<Delivery>> GetDeliveries(Guid vehicleId, int weekday);
+		Task<List<Depot>> GetDepots();
+		Task<List<Vehicle>> GetVehicles();
+		Task<List<Area>> GetAreas();
+	}
+}
