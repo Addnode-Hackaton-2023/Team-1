6e3b9481476ad373fed6c54139f84e646ab671f8
Niklas Almered <niklas.almered@decerno.se>
2023-08-30 20:38:48 +0200

    action

--- Allwin-Planning/Controllers/DriverController.cs
+++ Allwin-Planning/Controllers/DriverController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Allwin_Planning.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class DriverController : ControllerBase
     {
