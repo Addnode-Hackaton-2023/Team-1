11d7464f0a627984e42b4c50e2fdb2caa4cec77e
Love Lundquist <love.lundquist@decerno.se>
2023-08-30 18:50:05 +0200

    Adding stop log

---
+++ Allwin-Planning/Migrations/20230830164909_AddStopLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Allwin_Planning.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStopLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StopLog",
+                columns: table => new
+                {
+                    Gid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTimeUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StopId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StopType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StopLog", x => x.Gid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StopLog");
+        }
+    }
+}
