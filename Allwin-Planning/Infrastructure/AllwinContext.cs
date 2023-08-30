f88f8241d6a8acf02c413b8c564a16ac3f764842
Love Lundquist <love.lundquist@decerno.se>
2023-08-30 15:23:27 +0200

    adding context

---
+++ Allwin-Planning/Infrastructure/AllwinContext.cs
@@ -0,0 +1,17 @@
+﻿using Allwin_Planning.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Allwin_Planning.Infrastructure
+{
+    public class AllwinContext : DbContext
+    {
+        public AllwinContext(DbContextOptions<AllwinContext> options) : base (options) { }
+
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Depot> Depots { get; set; }
+        public DbSet<Pickup> Pickups { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyAllConfigurations(typeof(AllwinContext));
+    }
+}
---
+++ Allwin-Planning/Infrastructure/ModelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Allwin_Planning.Infrastructure
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ApplyAllConfigurations(this ModelBuilder modelBuilder, Type type)
+        {
+            var applyConfigurationMethodInfo = modelBuilder
+                .GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .First(m => m.Name.Equals("ApplyConfiguration", StringComparison.OrdinalIgnoreCase));
+
+
+
+            var ret = type.Assembly
+                .GetTypes()
+                .Select(t => (t, i: Array.Find(t.GetInterfaces(), i => i.Name.Equals(typeof(IEntityTypeConfiguration<>).Name, StringComparison.Ordinal))))
+                .Where(it => it.i != null)
+                .Select(it => (et: it.i.GetGenericArguments()[0], cfgObj: Activator.CreateInstance(it.t)))
+                .Select(it => applyConfigurationMethodInfo.MakeGenericMethod(it.et).Invoke(modelBuilder, new[] { it.cfgObj }))
+                .ToList();
+        }
+    }
+}
