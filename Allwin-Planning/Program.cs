521046f14a19eef1385471f98aa89272e0ebb4a2
Love Lundquist <love.lundquist@decerno.se>
2023-08-30 17:00:07 +0200

    Adding db context and migration

--- Allwin-Planning/Program.cs
+++ Allwin-Planning/Program.cs
@@ -1,11 +1,22 @@
+using Allwin_Planning.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connString = builder.Configuration["DefaultConnection"];
 
 // Add services to the container.
+builder.Services.AddDbContext<AllwinContext>(options =>
+options.UseSqlServer(connString, x => x.UseNetTopologySuite()));
+var dbBuilderContext = new DbContextOptionsBuilder<AllwinContext>();
+dbBuilderContext.UseSqlServer(connString, x => x.UseNetTopologySuite());
+using var context = new AllwinContext(dbBuilderContext.Options);
+context.Database.Migrate();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AllwinContext>();
 
 var app = builder.Build();
 
