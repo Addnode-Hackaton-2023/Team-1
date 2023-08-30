32f7cedc2986a2b9825e3d92c8b197d60eb7e81d
Love Lundquist <love.lundquist@decerno.se>
2023-08-30 20:35:52 +0200

    Adding GetVehicles endpoint

--- Allwin-Planning/Controllers/AdminController.cs
+++ Allwin-Planning/Controllers/AdminController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Allwin_Planning.Core.Entities;
+using Allwin_Planning.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Allwin_Planning.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<AdminController> _logger;
+        private readonly IAllwinRepository _repository;
+        public AdminController(ILogger<AdminController> logger, IAllwinRepository repository)
         {
-            return View();
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet(Name = "GetVehicles")]
+        public async Task<List<Vehicle>> GetVehicles()
+        {
+            return await _repository.GetVehicles();
         }
     }
 }
