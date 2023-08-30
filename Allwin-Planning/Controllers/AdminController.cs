daf78e306bf603b3022875bd96062763292b0855
Love Lundquist <love.lundquist@decerno.se>
2023-08-30 13:09:14 +0200

    gitignore

---
+++ Allwin-Planning/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Allwin_Planning.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Allwin-Planning/Controllers/RouteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Allwin_Planning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+    }
+}
