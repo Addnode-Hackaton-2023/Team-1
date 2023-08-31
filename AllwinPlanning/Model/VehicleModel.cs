f829833ee7f0988a99993c5588273d97d70859c9
Patrik Sjökvist <patrik.sjokvist@sgroup-solutions.se>
2023-08-31 10:23:32 +0200

    Added vechicleModel

---
+++ AllwinPlanning/Model/VehicleModel.cs
@@ -0,0 +1,27 @@
+﻿using AllwinPlanning.Core.Entities;
+using System.Linq.Expressions;
+
+namespace AllwinPlanning.Model
+{
+	public class VehicleModel
+	{
+		public Guid Gid { get; set; }
+		public string VehicleName { get; set; }
+		public int Capacity { get; set; }
+		public Guid DepotId { get; set; }
+		public List<PickupModel> Pickups { get; set; }
+		public List<DeliveryModel> Deliveries { get; set; }
+
+		public static Expression<Func<Vehicle, VehicleModel>> Projection => (v) => new VehicleModel
+		{
+			Gid = v.Gid,
+			VehicleName = v.VehicleName,
+			Capacity = v.Capacity,
+			DepotId = v.DepotId,
+			Pickups = v.Pickups.Select(PickupModel.Create).ToList(),
+			Deliveries = v.Deliveries.Select(DeliveryModel.Create).ToList()
+		};
+
+		public static VehicleModel Create(Vehicle d) => Projection.Compile().Invoke(d);
+	}
+}
