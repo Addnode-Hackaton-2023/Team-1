320474b657ff51d44120cd5a312bd6ba4a3dd379
Niklas Almered <niklas.almered@decerno.se>
2023-08-31 11:26:41 +0200

    GetActivePickups

--- AllwinPlanning/Infrastructure/Repository/AllWinRepository.cs
+++ AllwinPlanning/Infrastructure/Repository/AllWinRepository.cs
@@ -20,6 +20,13 @@ namespace AllwinPlanning.Infrastructure.Repository
                 .ToListAsync();
         }
 
+        public async Task<List<Pickup>> GetActivePickups(Guid vehicleId)
+        {
+            return await m_Context.Pickups
+                .Where(p => p.VehicleId == vehicleId && p.Active)
+                .ToListAsync();
+        }
+
         public async Task<List<Pickup>> GetAllPickups()
         {
             return await m_Context.Pickups.ToListAsync();
