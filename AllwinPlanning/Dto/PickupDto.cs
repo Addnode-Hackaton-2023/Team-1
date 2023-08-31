66243ae6864ae0c9cae487a065aca0595d62f4b0
Patrik Sjökvist <patrik.sjokvist@sgroup-solutions.se>
2023-08-31 09:21:58 +0200

    Add pickup dto and model

---
+++ AllwinPlanning/Dto/PickupDto.cs
@@ -0,0 +1,20 @@
+﻿using AllwinPlanning.Core;
+
+namespace AllwinPlanning.Dto
+{
+	public class PickupDto
+	{
+		public Guid Gid { get; set; }
+		public string PickupName { get; set; }
+		public string ContactName { get; set; }
+		public string ContactPhoneNumber { get; set; }
+		public int AvarageVolume { get; set; }
+		public int OpeningHour { get; set; }
+		public int ClosingHour { get; set; }
+		public int AvarageStoptime { get; set; }
+		public Guid VehicleId { get; set; }
+		public EsriPointDto Shape { get; set; }
+		public bool Active { get; set; } = true;
+		public PickupType PickupType { get; set; }
+	}
+}
