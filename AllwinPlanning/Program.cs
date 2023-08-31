7fbdc58d1dbc4ac7b3610cd53075d921aedcb48c
Marcus Alvén <marcus.alven@sgroup-solutions.se>
2023-08-31 08:37:21 +0200

    Added generated C# and typescript client

--- AllwinPlanning/Program.cs
+++ AllwinPlanning/Program.cs
@@ -15,6 +15,7 @@ using var context = new AllwinContext(dbBuilderContext.Options);
 context.Database.Migrate();
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,8 +27,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
