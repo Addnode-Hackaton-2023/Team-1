881eb370d5137c09a5e8efbe41019b1549f2d8d9
Patrik Sjökvist <patrik.sjokvist@sgroup-solutions.se>
2023-08-31 09:51:24 +0200

    Migrate to json points for drivercontroller

---
+++ AllwinPlanning/Model/StopsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AllwinPlanning.Model
+{
+	public class StopsModel
+	{
+		public DepotModel Depot { get; init; }
+
+		public IEnumerable<PickupModel> Pickups { get; init; }
+
+		public IEnumerable<DeliveryModel> Deliveries { get; init; }
+
+	}
+}
